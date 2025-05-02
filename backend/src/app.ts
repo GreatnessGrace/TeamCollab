7ba1312065aacc87b6b5dd038fc389c8041d3e26
GreatnessGrace <graceunstoppable7@gmail.com>
2025-05-02 14:48:29 +0530

    health api

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import dotenv from 'dotenv';
-
+import healthRoutes from './routes/health';
 
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/health', healthRoutes);
 
 export default app;
