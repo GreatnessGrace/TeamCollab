1b3153f6938607f844ee0c35a95ff5a552731ef0
GreatnessGrace <graceunstoppable7@gmail.com>
2025-05-05 12:42:52 +0530

    API: login

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,13 +1,17 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import healthRoutes from './routes/health';
+import authRoutes from './routes/auth.routes';
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use('/health', healthRoutes);
+app.use('/api/auth', authRoutes);
 
 export default app;
--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -12,3 +12,33 @@ export const register = async (req: Request, res: Response) => {
     const user = await User.create({ name, email, password });
     res.status(201).json({ message: 'User Registered', user: { id: user._id, name: user.name, email: user.email }})
 };
+
+
+export const login = async (req:Request, res:Response) => {
+    try {
+
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ email });
+        if(!user) return res.status(401).json({ message: 'Invalid email or password' });
+
+        const isMatch = await comparePasswords(password, user.password)
+
+        const payload = { userId: user._id, role: user.role };
+        const accessToken = signAccessToken(payload);
+        const refreshToken = signRefreshToken(payload)
+
+        res.cookie('refershToken', refreshToken, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: 'strict',
+            maxAge: 7 * 24 * 60 * 60 * 1000
+        });
+
+        return res.json({ accessToken })
+
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({ message: "Internal server error" });
+    }
+}
