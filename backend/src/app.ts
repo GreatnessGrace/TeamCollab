50669c7576260246a4333dcae62287acf857ad06
GreatnessGrace <graceunstoppable7@gmail.com>
2025-05-06 11:18:17 +0530

    Route: userRoutes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import healthRoutes from './routes/health';
 import authRoutes from './routes/auth.routes';
+import userRoutes from './routes/users.routes';
 import cookieParser from 'cookie-parser';
 
 dotenv.config();
@@ -13,5 +14,6 @@ app.use(cookieParser());
 
 app.use('/health', healthRoutes);
 app.use('/api/auth', authRoutes);
+app.use('/api/users', userRoutes);
 
 export default app;
