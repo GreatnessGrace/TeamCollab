48f2b7e08fb62bd5e2a4e00343c9ce0cb408072f
GreatnessGrace <graceunstoppable7@gmail.com>
2025-05-13 10:43:21 +0530

    Auth Controller Types

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -3,61 +3,81 @@ import { User } from "../models/user.model";
 import { comparePasswords } from "../utils/password";
 import { signAccessToken, signRefreshToken, verifyToken } from "../utils/jwt";
 
+export const register = async (req: Request, res: Response): Promise<void> => {
+  const { name, email, password } = req.body;
+  const existing = await User.findOne({ email });
+  if (existing) {
+    res.status(409).json({ message: "Email already in use" });
+    return;
+  }
 
-export const register = async (req: Request, res: Response) => {
-    const { name, email, password } = req.body;
-    const existing = await User.findOne({ email });
-    if (existing) return res.status(409).json({ message: 'Email already in use' });
-
-    const user = await User.create({ name, email, password });
-    res.status(201).json({ message: 'User Registered', user: { id: user._id, name: user.name, email: user.email }})
+  const user = await User.create({ name, email, password });
+  res
+    .status(201)
+    .json({
+      message: "User Registered",
+      user: { id: user._id, name: user.name, email: user.email },
+    });
 };
 
+export const login = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const { email, password } = req.body;
 
-export const login = async (req:Request, res:Response) => {
-    try {
-
-        const { email, password } = req.body;
-
-        const user = await User.findOne({ email });
-        if(!user) return res.status(401).json({ message: 'Invalid email or password' });
-
-        const isMatch = await comparePasswords(password, user.password)
-
-        const payload = { userId: user._id, role: user.role };
-        const accessToken = signAccessToken(payload);
-        const refreshToken = signRefreshToken(payload)
-
-        res.cookie('refershToken', refreshToken, {
-            httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
-            sameSite: 'strict',
-            maxAge: 7 * 24 * 60 * 60 * 1000
-        });
-
-        return res.json({ accessToken })
-
-    } catch (error) {
-        console.log(error)
-        return res.status(500).json({ message: "Internal server error" });
+    const user: any = await User.findOne({ email });
+    if (!user) {
+      res.status(401).json({ message: "Invalid email or password" });
+      return;
     }
-}
 
-export const refreshAccessToken = async (req: Request, res: Response) => {
-    try {
-      const token = req.cookies.refershToken;
-      if (!token) {
-        return res.status(401).json({ message: 'Refresh token missing' });
-      }
-  
-      const decoded = verifyToken(token, 'refresh') as { userId: string; role: string };
-  
-      const accessToken = signAccessToken({ userId: decoded.userId, role: decoded.role });
-  
-      return res.json({ accessToken });
-  
-    } catch (err) {
-      console.error('Refresh token error:', err);
-      return res.status(401).json({ message: 'Invalid refresh token' });
+    const isMatch: boolean = await comparePasswords(password, user.password);
+
+    const payload = { userId: user._id, role: user.role };
+    const accessToken = signAccessToken(payload);
+    const refreshToken = signRefreshToken(payload);
+
+    res.cookie("refershToken", refreshToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+
+    res.json({ accessToken });
+    return;
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal server error" });
+    return;
+  }
+};
+
+export const refreshAccessToken = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const token = req.cookies.refershToken;
+    if (!token) {
+      res.status(401).json({ message: "Refresh token missing" });
+      return;
     }
-  };
+
+    const decoded = verifyToken(token, "refresh") as {
+      userId: string;
+      role: string;
+    };
+
+    const accessToken = signAccessToken({
+      userId: decoded.userId,
+      role: decoded.role,
+    });
+
+    res.json({ accessToken });
+    return;
+  } catch (err) {
+    console.error("Refresh token error:", err);
+    res.status(401).json({ message: "Invalid refresh token" });
+    return;
+  }
+};
