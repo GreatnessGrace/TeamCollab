21a9fc0d3b173bfe4b7f869d1db510d4f807e294
GreatnessGrace <graceunstoppable7@gmail.com>
2025-05-13 10:43:30 +0530

    User Controller Types

--- backend/src/controllers/users.controller.ts
+++ backend/src/controllers/users.controller.ts
@@ -2,10 +2,15 @@ import { Request, Response } from "express";
 import { User } from "../models/user.model";
 import { AuthenticatedRequest } from "../middlewares/auth.middleware";
 
-export const getProfile = async (req: AuthenticatedRequest, res: Response) => {
-    const user = await User.findById(req.user?.userId).select('-password');
-    if (!user) return res.status(404).json({ message: 'User not found' });
-  
-    res.json(user);
-  };
-  
+export const getProfile = async (
+  req: AuthenticatedRequest,
+  res: Response
+): Promise<void> => {
+  const user = await User.findById(req.user?.userId).select("-password");
+  if (!user) {
+    res.status(404).json({ message: "User not found" });
+    return;
+  }
+
+  res.json(user);
+};
